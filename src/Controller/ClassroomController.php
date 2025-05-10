1b7cdb43498c40b77917d062956406ba67a219de
HopefulUndead <152425764+HopefulUndead@users.noreply.github.com>
2025-05-10 17:43:29 +0200

    refactor /myClassroom -> classroom/index.php

--- src/Controller/ClassroomController.php
+++ src/Controller/ClassroomController.php
@@ -16,34 +16,39 @@
     #[IsGranted('ROLE_USER', statusCode: 404)]
     final class ClassroomController extends AbstractController
     {
-        #[Route('/classroom/{id}', name: 'app_classroom')]
-        public function index(int $id, Security $security, TaskRepository $taskRepository, ClassroomRepository $classroomRepository, UserRepository $userRepository): Response
+        #[Route('/classroom', name: 'classroom_index')]
+        public function index(Security $security, ClassroomRepository $ClassroomRepository): Response
+        {
+            $user = $security->getUser();
+            $classrooms = $ClassroomRepository->findByUser($user->getId());
+
+            #dd($classrooms);
+            return $this->render('classroom/index.html.twig', [
+                'classrooms' => $classrooms,
+            ]);
+        }
+
+            #[Route('/classroom/{id}', name: 'classroom_show')]
+        public function show(int $id, Security $security, TaskRepository $taskRepository, ClassroomRepository $classroomRepository, UserRepository $userRepository): Response
         {
             $user = $security->getUser();
 
-            //
             if ( $classroomRepository->userIsInClass(
                 $user->getId(),
                 $id)
             === false)
-            throw $this->createAccessDeniedException("You are not allowed to access this page : you arent in this classroom");
+            throw $this->createNotFoundException("You are not allowed to access this page : you arent in this classroom");
 
             $tasks =  $taskRepository->findByClassroom($id);
-            #dd($tasks);
-
-
             $classroom = $classroomRepository->findById($id);
             $teacher = $userRepository->findById($classroom->getIdTeacher());
-
             $studentsInClass = $userRepository->findByClassroom($id); // all users
-
             // permet d'enlever LE prof, afin d'avoir les élèves
             $studentsInClass = array_filter($studentsInClass, fn($obj) => $obj !== $teacher);
             $studentsInClass = array_values($studentsInClass); // réindexe
-
             $isTeacher = $user->getId() === $teacher->getId();
 
-            return $this->render('classroom/index.html.twig', [
+            return $this->render('classroom/show.html.twig', [
                 'tasks' => $tasks,
                 'students' => $studentsInClass,
                 'classroom' => $classroom,
